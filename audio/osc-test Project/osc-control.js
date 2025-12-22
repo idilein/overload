24f04c7187b1991d15f69f70ac6b48bf99f41cf5
Norbert Schnell <norbert@schnell.space>
2025-12-22 13:10:31 +0100

    added max4live osc link

---
+++ audio/osc-test Project/osc-control.js
@@ -0,0 +1,22 @@
+autowatch = 1;
+outlets = 1;
+
+var liveObject = new LiveAPI();
+
+function start_scene(sceneIndex) {
+  // post('start_scene', sceneIndex, '\n');
+  liveObject.path = 'live_set scenes ' + sceneIndex;
+  liveObject.call('fire');
+}
+
+
+function start_clip(trackIndex, clipIndex) {
+  liveObject.path = 'live_set tracks ' + trackIndex + ' clip_slots ' + clipIndex;
+  liveObject.call('fire');
+}
+
+function stop_all() {
+  liveObject.path = 'live_set';
+  liveObject.call('stop_all_clips', 0);
+  liveObject.call('stop_playing');
+}
